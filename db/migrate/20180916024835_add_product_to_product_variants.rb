0bcccb70a7e845b4b283e6f3686d2d7748152092
Mario Keophilalay <yanndyna@gmail.com>
2018-09-30 04:04:29 +0000

    delete schema

--- db/migrate/20180916024835_add_product_to_product_variants.rb
+++
@@ -1,5 +0,0 @@
-class AddProductToProductVariants < ActiveRecord::Migration
-  def change
-    add_reference :product_variants, :product, index: true, foreign_key: true
-  end
-end
--- db/migrate/20180916025916_add_order_item_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddOrderItemToOrders < ActiveRecord::Migration
-  def change
-    add_reference :order_items, :order, index: true, foreign_key: true
-  end
-end
