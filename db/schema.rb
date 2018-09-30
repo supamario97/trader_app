2b0e543042d15be0f4bf2ed67fc929a506f0731e
Mario Keophilalay <yanndyna@gmail.com>
2018-09-30 14:11:50 +0000

    added shopify

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,58 @@
 
 ActiveRecord::Schema.define(version: 20180930034205) do
 
-  
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer  "quantity",                            default: 0, null: false
+    t.decimal  "price",      precision: 15, scale: 2,             null: false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.integer  "order_id"
+  end
+
+  add_index "order_items", ["order_id"], name: "index_order_items_on_order_id"
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name",                           null: false
+    t.decimal  "sub_total",  precision: 15, scale: 2, null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.integer  "product_id",  null: false
+    t.integer  "category_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "product_categories", ["category_id"], name: "index_product_categories_on_category_id"
+  add_index "product_categories", ["product_id"], name: "index_product_categories_on_product_id"
+
+  create_table "product_variants", force: :cascade do |t|
+    t.string   "title",                               null: false
+    t.decimal  "price",      precision: 15, scale: 2, null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.integer  "product_id"
+  end
+
+  add_index "product_variants", ["product_id"], name: "index_product_variants_on_product_id"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title",                                null: false
+    t.text     "description"
+    t.decimal  "price",       precision: 15, scale: 2, null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+  end
+
   create_table "shops", force: :cascade do |t|
     t.string   "shopify_domain", null: false
     t.string   "shopify_token",  null: false
