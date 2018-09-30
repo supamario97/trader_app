5e84aba1a3cba3ebde9db34631310d227d15b1fc
Mario Keophilalay <yanndyna@gmail.com>
2018-09-30 15:59:09 +0000

    added images products

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,5 +3,5 @@ ShopifyApp.configure do |config|
   config.api_key = "<api_key>"
   config.secret = "<secret>"
   config.scope = "read_orders, read_products"
-  config.embedded_app = false
+  config.embedded_app = true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
   
-  root :to => 'home#index'
-  mount ShopifyApp::Engine, at: '/'
-  #root 'pages#home'
+  
+ 
+  root 'pages#home'
 
   get 'about', to:'pages#about'
 
